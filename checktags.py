94d254f4823d270348667543ef765d01f0bcba68
sschlott <43354996+sschlott@users.noreply.github.com>
2019-10-17 15:53:33 -0700

    secondary try/catch for valueerrors and comments

--- checktags.py
+++ checktags.py
@@ -13,14 +13,11 @@ import os.path
 
 def CheckTags():
     """Reads a URL from the user and then checks it for tag matching."""
-    # url = input("URL: ")
-    url = 'http://people.reed.edu/~esroberts/index.html'
+    url = input("URL: ")
     base = os.path.dirname(url)
     checkURL(url,base)
 def checkURL(url,base):
     """Checks whether the tags are balanced in the specified URL."""
-    # response = urllib.request.Request(url)
-
     try: 
         response = urllib.request.urlopen(url)
         print("!",type(response))
@@ -30,6 +27,7 @@ def checkURL(url,base):
     except urllib.error.URLError as e: 
         print("Encountered a problem:", e.getcode(), e.reason)
 # From https://python.readthedocs.io/en/stable/howto/urllib2.html
+#Part 1: Checks for errors and reports problems with provided link
 
 
 
@@ -65,14 +63,19 @@ class HTMLTagParser(html.parser.HTMLParser):
         self.feed(text)
         while len(self.linkStack) > 0:
             
-            #attempt to parse, if error, print. otherwise, move on
+            #attempt to parse, if error, print. otherwise, move on. ValueError means 
+            #there might be a partial link from the root of the webpage, so try with that
             try:
                 tag, line, link = self.linkStack.pop()
                 response = urllib.request.urlopen(link)
             except urllib.error.URLError as e: 
                 print("Encountered a problem on line "+str(line)+ " with tag <"+str(tag)+ "> for link "+str(link)+"\n"+ str(e.reason))
             except ValueError:
-                response = urllib.request.urlopen(base+"/"+link)
+                try: 
+                    response = urllib.request.urlopen(base+"/"+link)
+                except:
+                    print("Encountered a problem on line "+str(line)+ " with tag <"+str(tag)+ "> for short link "+str(link)+"\n"+ str(e.reason))
+
     def handle_starttag(self, startTag, attributes):
         """Overrides the callback function for start tags."""
         startLine,_ = self.getpos()
